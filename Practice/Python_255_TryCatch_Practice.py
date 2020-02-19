d7c01731d15749fb25a359fdc63064e12c764a6c
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-02-18 16:23:57 -0800

    Update Python_255_TryCatch_Practice.py

--- Practice/Python_255_TryCatch_Practice.py
+++ Practice/Python_255_TryCatch_Practice.py
@@ -1,10 +1,13 @@
-while True:
-    try:
-        a = int(input("Tell me one number:"))
-        if (a < -1 or a > 10):
-            print("Number exceed range, try again")
-        elif a in range(1,10): 
-            break      
-    except:        
-        print("Invalid input, only number allow")
-print(str(a))
+def getNumber():
+    while True:
+        try:
+            a = int(input("Tell me one number:"))
+            if (a < -1 or a > 10):
+                print("Number exceed range(1,10), try again")
+            elif a in range(1,10): 
+                return a     
+        except ValueError: #Specific type of eroor, the interpreter will notice ValueError when it occur       
+            print("Invalid input, only number allow")
+        except: #Anytype of error, the intepreter won't tell the user
+            print("Something wrong happened, try again")
+print("Number enter:" + str(getNumber()))

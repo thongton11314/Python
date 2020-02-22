3544b444c8e4d023cab934705d0d7cbf09bb26fa
Thong Ton <t.ton8789@edmail.edcc.edu>
2020-02-21 20:28:35 -0800

    Update Python_255_FInal Project.py

---
+++ Practice/Python_255_FInal Project.py
@@ -0,0 +1,14 @@
+# Option 1 - Data Analysis
+# Objectives
+# 	Understand how to read files using Python
+# 	Use list, dictionary, and programming elements to analyze the data from the file
+# 	Use Json package to read Json from a file
+# 	Use Mathplotlib to draw a scatter plot
+# Instruction
+# 	Download the data file from the Canvas
+# 	Import json library to read data into a list of records
+# 	Use list, dictionary, and programming elements to display 10 ten regions (timezone) that use the Internet
+# 	Draw a horizontal bar chart to display the top ten regions and scatter plot of the data.
+# Submission
+# 	The py source code
+# 	Snapshots of the scatter plot and the horizontal bar chart

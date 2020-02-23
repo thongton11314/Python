b012886fba1c25ede204e2c73583f78c225d36fe
Thong Ton <t.ton8789@edmail.edcc.edu>
2020-02-23 00:33:26 -0800

    AA

--- Practice/Python_255_Nested Dictionary_And_Using JSON.py
+++ Practice/Python_255_Nested Dictionary_And_Using JSON.py
@@ -3,6 +3,51 @@
 
 import json
 
+# This function will write data to txt file
+# Input:
+#   fileName: the name of the file
+#   data: the data from user
+# Output:
+#   a file has the [fileName.txt] with [data] in it
+def writeToJSON(fileName, data):
+    myFile = None
+    #Open file
+    try:
+        myFile = open(fileName, 'w')
+        json.dump(data, myFile)
+    #Any error relative to file
+    except IOError:
+        print("File is not found") 
+    #Close file
+    finally:
+        if myFile is not None:
+            myFile.close()
+        else:
+            print("There is no file to close")  
+
+# This function will read data to json format
+# Input:
+#   fileName: the name of the file
+# Output:
+#   a dictionary filed by data from [filename]
+def readingFromJSON(fileName):
+    myFile = None
+    #Open file
+    try:
+        with open(fileName) as myFile:
+            data = json.load(myFile)
+        return data
+    #Any error relative to files
+    except IOError:
+        print("File is not found")
+    #Close file
+    finally:
+        if myFile is not None:
+            myFile.close()
+        else:
+            print("There is no file to close")
+
+#Maked up data
 peopleInfor = { 
     "Person 1" : { 
         "Name" : "Thong",
@@ -24,10 +69,16 @@ peopleInfor = {
     } 
 }
 
-for eachPerson in peopleInfor:
-    print("Name:" + peopleInfor[eachPerson].get("Name") 
-        + " - Year:" + str(peopleInfor[eachPerson].get("Year")) 
-        + " - National:" + peopleInfor[eachPerson].get("National")
-        + " - Good at:" + str(peopleInfor[eachPerson].get("Good at"))
+#File name
+fileName = "JSON_Test.txt"
+#Write data to file 
+writeToJSON(fileName, peopleInfor)
+#Print data to file
+print("Test reading from file")
+data = readingFromJSON(fileName)
+for eachPerson in data:
+    print("Name:" + data[eachPerson].get("Name") 
+        + " - Year:" + str(data[eachPerson].get("Year")) 
+        + " - National:" + data[eachPerson].get("National")
+        + " - Good at:" + str(data[eachPerson].get("Good at"))
     )
-    

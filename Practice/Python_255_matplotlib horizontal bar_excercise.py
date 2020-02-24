aec3b9ee55b63eeda947c744e5c29a21d333598a
Thong Ton <t.ton8789@edmail.edcc.edu>
2020-02-24 14:48:46 -0800

    Update Python_255_matplotlib horizontal bar_excercise.py

--- Practice/Python_255_matplotlib horizontal bar_excercise.py
+++ Practice/Python_255_matplotlib horizontal bar_excercise.py
@@ -3,8 +3,17 @@
 #Windows (may require elevation)
 #python -m pip install matplotlib
 
-import matplotlib as plt
 import numpy as np
-data = {"Vietnam": 50, "United State": 100, "England": 75, "Lao": 25}
-y_pos = np.arrage(len(data))
-plt.barh(y_pos, data)
+import matplotlib.pyplot as plt
+
+# Make fake dataset
+data = {"United State": 100, "England": 75, "Vietnam": 50, "Lao": 25}
+rank = [data["United State"], data["England"], data['Vietnam'], data["Lao"]]
+bars = ('United State', 'England', 'VietNam', 'Lao')
+y_pos = np.arange(len(bars))
+# Create horizontal bars
+plt.barh(y_pos, rank)
+# Create names on the y-axis
+plt.yticks(y_pos, bars)
+# Show graphic
+plt.show()

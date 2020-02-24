79d4bb961ce48a178761c36b1796336e053476d1
Thong Ton <t.ton8789@edmail.edcc.edu>
2020-02-24 14:40:51 -0800

    a

--- Practice/Python_255_matplotlib horizontal bar_excercise.py
+++ Practice/Python_255_matplotlib horizontal bar_excercise.py
@@ -1,3 +1,10 @@
-import matplotlib
+#py -3 -m venv .venv
+#.venv\scripts\activate
+#Windows (may require elevation)
+#python -m pip install matplotlib
 
+import matplotlib as plt
+import numpy as np
 data = {"Vietnam": 50, "United State": 100, "England": 75, "Lao": 25}
+y_pos = np.arrage(len(data))
+plt.barh(y_pos, data)

b5bedad6167adba286e9e5b85b817e9e1f3a85c3
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-02-25 15:44:49 -0800

    l

---
+++ Practice/Python_255_Comprehensive_Practice_Part2.py
@@ -0,0 +1,25 @@
+def square():
+    result = []
+    for a in range(1,30):
+        for b in range(1,30):
+            for c in range(1,30):
+                if (a**2 + b**2 == c**2):
+                    result.append((a,b,c))
+    return result
+
+def square2():
+    #Return ... For loop ... Condition Statement
+    return [(a,b,c) for a in range(1,30) for b in range(1,30) for c in range(1,30) if a**2 + b**2 == c**2]
+
+
+def isSquare(a,b,c):
+        return a**2 + b**2 == c**2
+
+def square3():    
+    return [(a,b,c) for a in range(1,30) for b in range(1,30) for c in range(1,30) if isSquare(a,b,c)]
+
+
+
+print(square())
+print(square2())
+print(square3())
--- Practice/Python_255_Read Each JSON_Practice.py
+++ Practice/Python_255_Read Each JSON_Practice.py
@@ -1,16 +1,24 @@
 def readFile(fileName):
-    myFile = None
+    myFile = None   
+    list = []
     try:
-        myfile = open(fileName, 'r')     
-        return myfile.readlines()
+        myfile = open(fileName, 'r')
+        while True:
+            line = myfile.readline()
+            if line.strip():
+                list.append(line.strip())
+            if not line:
+                break
+        return list
     except IOError:
         print("File is not found")
+        return []
     finally:
         if myFile is not None:
             myFile.close()
         else:
             print("There is no file to close")
 
-fileName= "National Internet Using.json"
+fileName= "Test File Multiple Line.txt"
 print(type(readFile(fileName)))
 print(readFile(fileName))

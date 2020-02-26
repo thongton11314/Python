d56518a52761a89031759e8eae507a31bd6d67f5
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2020-02-25 16:26:21 -0800

    A

--- Practice/Python_255_List comprehension_Practice.py
+++ Practice/Python_255_List comprehension_Practice.py
@@ -1,6 +1,13 @@
-#List comprehension
+import math
+#List comprehension Practice
+#Using map function, map(function, interator)
+
 mypets = ["dog", "cat", "fish"]
 myUpperPets = list(map(str.upper, mypets))
 anotherPets = [pet.upper() for pet in mypets]
 print(myUpperPets)
 print(anotherPets)
+
+number = [0.555, 0.355, 0.999, 0.445]
+roundUpper = list(map(round, number))
+print(roundUpper)

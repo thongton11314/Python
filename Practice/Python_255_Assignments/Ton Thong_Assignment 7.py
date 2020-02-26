a39df256f0644ba27d744133deb471c285895064
Thong Ton <t.ton8789@edmail.edcc.edu>
2020-02-25 21:15:51 -0800

    Update Ton Thong_Assignment 7.py

--- Practice/Python_255_Assignments/Ton Thong_Assignment 7.py
+++ Practice/Python_255_Assignments/Ton Thong_Assignment 7.py
@@ -30,16 +30,9 @@ def decode(sentence, distance):
                 result += chr((ord(char) - distance - 97) % 26 + 97)
     return result
 
-print(decode(encode("Thong Ton", 5), 5))
-print(decode(encode("Thanh Ton", 5), 5))
 print(decode(encode("Diep Ton", 5), 5))
-print(decode(encode("Bao Tran", 5), 5))
-print(decode(encode("Linh Nguyen", 5), 5))
-print(decode(encode("Nghia Le", 5), 5))
-print(decode(encode("Thinh Le", 5), 5))
-
-
-
-
-
-
+print(decode(encode("Thanh Ton", 5), 5))
+print(decode(encode("Thong Ton", 5), 5))
+print(decode(encode("Adnrew Ton", 5), 5))
+print(decode(encode("Micheal Ton", 5), 5))
+print(decode(encode("Stephen Ton", 5), 5))

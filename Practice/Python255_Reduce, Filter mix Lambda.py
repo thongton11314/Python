e95ea6efc5f3e33035c7c81e5c5ddb434a79317f
Thong Ton <t.ton8789@edmail.edcc.edu>
2020-02-27 23:52:49 -0800

    Commit

--- Practice/Python255_Reduce, Filter mix Lambda.py
+++ Practice/Python255_Reduce, Filter mix Lambda.py
@@ -1,20 +1,22 @@
 import functools
 
 # Testing lambda expression
-# Result in lambda location in memory
+# Result in lambda location in memory (value = address of this function)
 print((lambda a,b: a + b))
-# Result the lambda expression
-print((lambda a,b: a + b)(1, 2))
-
+# Result the lambda expression, (value = 3)
+print((lambda a,b: a + b)(1, 2)) 
+# Another example, (value = 1)
+result = lambda x: x*x*x 
+print(result(1))
 
 mylist = list(range(-10, 10))
 
 # Print number less than 10 by using fillter
 # The filter() method filters the given sequence with the help of a function that tests each element in the sequence to be true or not
 # filter(fun(), sequence) 
-# Result in filter adress
+# Result in filter adress (value = address of this function)
 print(filter(lambda a: a < 0, mylist))
-# Result in list of element after filter 
+# Result in list of element after filter, (value = all number less than 0)
 print(list(filter(lambda a: a < 0, mylist)))
 
 # Print the average by using reduce
@@ -23,5 +25,6 @@ print(list(filter(lambda a: a < 0, mylist)))
 # This process continues till no more elements are left in the container.
 # The final returned result is returned and printed on console. 
 # reduce(fun(), sequence) 
+# Result in (value = average)
 print(functools.reduce(lambda a, b: a + b, mylist) / len(mylist))
 
